76c0bcf01aeec63eb83518a09c28dd643a3c9432
Brent Reynolds <brentsreynolds5@gmail.com>
2023-06-03 17:56:46 -0400

    Word counter

--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {
-        'name': 'Brent',
-        'age': 20,
-        'nationality': 'American',
-    }
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
