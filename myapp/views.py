122a0d015821657d23d8fe3d67c49afda40a0862
Brent Reynolds <brentsreynolds5@gmail.com>
2023-06-03 18:16:31 -0400

    POST request

--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,6 @@ def index(request):
     return render(request, 'index.html')
 
 def counter(request):
-    text = request.GET['text']
+    text = request.POST['text']
     amount_of_words = len(text.split())
     return render(request, 'counter.html', {'amount': amount_of_words})
